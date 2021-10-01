8067bae7b17c56738c790a6c2fe96a97e81efa86
GabrieSotelo97 <87153830+GabrieSotelo97@users.noreply.github.com>
2021-10-01 00:06:46 -0300

    sube mas productos,arreglo de rutas y rutas dinamicas

--- site/src/app.js
+++ site/src/app.js
@@ -22,9 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', main);
-app.use('/', product)
+app.use('/product', product)
 app.use('/user', user);
-app.use('/', admin)
+app.use('/admin', admin)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
