2cbd0553e6dddbf6729901916ccae3330003f6f4
GabrieSotelo97 <87153830+GabrieSotelo97@users.noreply.github.com>
2022-01-03 17:23:43 -0300

    sube previsualizacion de imagen al carga y edit de producto y register y edit de usuario

--- site/public/javascripts/register.js
+++ site/public/javascripts/register.js
@@ -254,3 +254,21 @@ window.addEventListener('load',()=>{
 
     
 })
+document.getElementById("image").onchange = (e)=> {
+    // Creamos el objeto de la clase FileReader
+    let reader = new FileReader();
+  
+    // Leemos el archivo subido y se lo pasamos a nuestro fileReader
+    reader.readAsDataURL(e.target.files[0]);
+  
+    // Le decimos que cuando este listo ejecute el código interno
+    reader.onload = function(){
+      let preview = document.getElementById('preview3'),
+              image = document.createElement('img');
+  
+      image.src = reader.result;
+  
+      preview.innerHTML = '';
+      preview.append(image);
+    };
+  }
