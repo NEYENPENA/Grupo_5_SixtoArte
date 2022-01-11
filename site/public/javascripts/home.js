7147df4b86589d25be524a6efe51873caee4e829
GabrieSotelo97 <87153830+GabrieSotelo97@users.noreply.github.com>
2022-01-11 00:46:15 -0300

    arregla modo oscuro del home

--- site/public/javascripts/home.js
+++ site/public/javascripts/home.js
@@ -50,42 +50,60 @@ window.addEventListener('load',()=>{
 
 
 
+    let main = document.querySelector(".home");
+    
+    let p2 = document.querySelectorAll("p");
+    let button = document.querySelectorAll("button");
+    let carrito = document.querySelectorAll('.fa-shopping-cart')
+    /* const darkGray = "#222426";
+    const darker = "#4592AF";
+    const white = "#FFFFFF";
+    
+    const whiteGray = "#FFFFFF";
+    
+    function changeMode (mode = "dark"){
+      
+    
+      p2.forEach((e) => {
+        e.style.color = mode == "dark" ? white : darker;
+      });
+      main.style.backgroundColor = mode == "dark" ? darkGray : white;
+      p.style.color = white;
+      button.forEach((e) => {
+        e.style.backgroundColor = mode == "dark" ? darker : white;
+        e.style.borderRadius = "5px";
+      });
+    }; */
+    
+    
+    
+      let darkModeButton = document.querySelector(".fa-adjust");
+    
+      /* darkModeButton.addEventListener("click", function() {
+            // next two code lines should be replaced by store functions
+        let main = document.querySelector(".home");
+        if (main.style.backgroundColor == darkGray) {
+          changeMode("light");
+        } else {
+          changeMode("dark");
+        }
+      }, false); */
+      darkModeButton.addEventListener('click',()=>{
+        main.classList.toggle('fondoNegro')
+
+        p2.forEach(parrafo =>{
+          parrafo.classList.toggle('blanco')
+        })
+        carrito.forEach(car =>{
+          car.classList.toggle('blanco') 
+        })
+        carrito.forEach(car =>{
+          car.classList.toggle('fondoNegro') 
+        })
+        
+      })
+    
+
 
 })
-const darkGray = "#222426";
-const darker = "#4592AF";
-const white = "#FFFFFF";
 
-const whiteGray = "#FFFFFF";
-
-function changeMode (mode = "dark"){
-  let main = document.querySelector(".home");
-  let p = document.querySelector(".precio");
-  let p2 = document.querySelectorAll("p");
-  let button = document.querySelectorAll("button");
-
-  p2.forEach((e) => {
-    e.style.color = mode == "dark" ? white : darker;
-  });
-  main.style.backgroundColor = mode == "dark" ? darkGray : white;
-  p.style.color = white;
-  button.forEach((e) => {
-    e.style.backgroundColor = mode == "dark" ? darker : white;
-    e.style.borderRadius = "5px";
-  });
-};
-
-
-window.onload = function () {
-  const darkModeButton = document.querySelector(".icons");
-
-  darkModeButton.addEventListener("click", function() {
-        // next two code lines should be replaced by store functions
-    let main = document.querySelector(".home");
-    if (main.style.backgroundColor == darkGray) {
-      changeMode("light");
-    } else {
-      changeMode("dark");
-    }
-  }, false);
-};
