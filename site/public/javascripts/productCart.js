1544850884e7aaeef2691e1b20f9db00e3ee9859
Maikel Esquivel <marcostorres04031997@gmail.com>
2022-01-11 22:26:55 -0300

    bordeando formularios

--- site/public/javascripts/productCart.js
+++ site/public/javascripts/productCart.js
@@ -1,11 +1,14 @@
 window.onload = function () {
     
 const main = document.querySelector('.carritoDeProducto')
-let p = document.querySelector('.resultadofinal')
-let p2 = document.querySelector('.precio')
+
+
+let p2 = document.querySelectorAll('.precio')
 let button = document.querySelector('.pagar')
 let p3 = document.querySelector('.producto')
-const div = document.querySelector('.icons')
+let body = document.querySelector('body')
+
+let darkModeButton = document.querySelector(".botonDark");
 
 /* const darkmode = confirm('¿Desea modo oscuro?') */
 
@@ -18,14 +21,15 @@ p2.style.backgroundColor = '#808080'
 button.style.backgroundColor = '#4592AF'
 p3.style.color = '#FFFFFF'
    }  */
-   div.addEventListener("click", (e) => {
-      main.style.backgroundColor = '#222426'
-      p.style.color = '#FFFFFF'
-      p2.style.color = '#FFFFFF'
-      p2.style.backgroundColor = '#808080'
+  darkModeButton.addEventListener("click", () => {
+      main.style.backgroundColor = '#2e2d2d'
+      p2.forEach(parrafo =>{
+         parrafo.style.color = '#FFFFFF'
+       })
       button.style.backgroundColor = '#4592AF'
       button.style.color = '#FFFFFF'
       p3.style.color = '#FFFFFF'
+      body.style.backgroundColor = '#747272'
      })
 
 }
